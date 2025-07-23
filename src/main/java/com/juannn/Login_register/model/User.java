069537a506ea43628b3d057b3a2d5af2f70738b8
juandda0 <juandiegodiazave@gmail.com>
2025-07-23 11:46:46 -0500

    getter and setter entity user

--- src/main/java/com/juannn/Login_register/model/User.java
+++ src/main/java/com/juannn/Login_register/model/User.java
@@ -47,8 +47,52 @@ public class User implements UserDetails {
         return email;
     }
 
+    public UUID getId() {
+        return id;
+    }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
 
